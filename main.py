385862ef7425455ff923048352f6e6a806399eed
ashwinfernandes <ashtroid06@gmail.com>
2025-02-16 00:59:00 +0100

    My first set of learnings

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print('Hello, World!')
+    print(1+2)
+    print(7*6)
+    print()
+    print("The end", "or ist it?", "keep watching to learn more about Python", 3)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ strings.py
@@ -0,0 +1,22 @@
+print("today is a good day to learn python")
+print('python is fun')
+print("python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = "Ashwin"
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(f'{name} is {age} years old"')
+print(type(age_in_words))
