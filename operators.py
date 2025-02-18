6b58226a4771fc0b5747bd505264da8e3801a971
ashwinfernandes <ashtroid06@gmail.com>
2025-02-18 15:16:56 +0100

    learn some more about operators precedence

--- operators.py
+++ operators.py
@@ -6,3 +6,19 @@ print(a - b)  # 9
 print(a * b)  # 36
 print(a / b)  # 4.0
 print(a // b)  # 4 integer division, rounded down to
+
+print()
+
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+print(((a + b) / 3) - (4 * 12))
+
+c = a + b
+d = c / 3
+e = d -4
+print(e * 12)
+
+print()
+
+print(a / (b * a) / b)
