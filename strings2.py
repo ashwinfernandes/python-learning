8cca90d144f94c7944c26feb8f3e64ec0073f340
ashwinfernandes <ashtroid06@gmail.com>
2025-02-19 20:50:22 +0100

    slicing with start and stop

--- strings2.py
+++ strings2.py
@@ -1,44 +1,27 @@
 # Indexing
 #         01234567890123
 parrot = "Norwegian Blue"
-print(parrot)
 
-###### mini challenge ######
-# what_i_want = "we win"
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[0:6]) # Norweg
+print(parrot[3:5]) # we
+print(parrot[0:9]) # Norwegian
+print(parrot[:9]) # Norwegian
 
-### my way ###
-# what_i_want = "we win"
-# for char in what_i_want:
-#     index = parrot.find(char)
-#     print(parrot[index])
+print(parrot[10:14]) # Blue
+print(parrot[10:]) # Blue
 
-print()
-# what_i_want = "we win" using negative indexing
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+print(parrot[:6])
+print(parrot[6:])
 
-### my way ###
-# what_i_want = "we win" using negative indexing
-# what_i_want = "we win"
-# for char in what_i_want:
-#     index = parrot.find(char)
-#     print(parrot[index-len(parrot)])
+print(parrot[:6] + parrot[6:])
 
-# another way
-stringLen = len(parrot)
-print(parrot[3 - stringLen])
-print(parrot[4 - stringLen])
-print(parrot[9 - stringLen])
-print(parrot[3 - stringLen])
-print(parrot[6 - stringLen])
-print(parrot[8 - stringLen])
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters[:4])
+print(letters[4:7])
+print(letters[7:11])
+print(letters[11:16])
+print(letters[16:20])
+print(letters[20:23])
+print(letters[23:])
