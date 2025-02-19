5d0a833d970fd80dc459a66a2f772cd6ec101637
ashwinfernandes <ashtroid06@gmail.com>
2025-02-19 21:13:00 +0100

    negative slicing

--- strings2.py
+++ strings2.py
@@ -3,25 +3,18 @@
 parrot = "Norwegian Blue"
 
 print(parrot[0:6]) # Norweg
-print(parrot[3:5]) # we
-print(parrot[0:9]) # Norwegian
-print(parrot[:9]) # Norwegian
 
-print(parrot[10:14]) # Blue
-print(parrot[10:]) # Blue
+print(parrot[-4:-2]) # Bl
+print(parrot[-4:12]) # Bl
 
-print(parrot[:6])
-print(parrot[6:])
+print(parrot[-14:-8]) #Norweg
+print(parrot[-14:-8]) #Norweg
 
-print(parrot[:6] + parrot[6:])
+print(parrot[-11:-9]) #we
+print(parrot[:-5]) #Norwegian
+print(parrot[-14:-5]) #Norwegian
 
-print(parrot[:])
+print(parrot[-4:14])
+print(parrot[-4:])
 
-letters = "abcdefghijklmnopqrstuvwxyz"
-print(letters[:4])
-print(letters[4:7])
-print(letters[7:11])
-print(letters[11:16])
-print(letters[16:20])
-print(letters[20:23])
-print(letters[23:])
+print(parrot[:-5] + parrot[-5:])
