37a3881a9130f7b1f84791e8e9de225a94966236
ashwinfernandes <ashtroid06@gmail.com>
2025-04-16 21:46:55 +0200

    variable naming convention

--- escapechar.py
+++ escapechar.py
@@ -1,8 +1,8 @@
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
+split_string = "This string has been\nsplit over\nseveral\nlines"
+print(split_string)
 
-tabbedString = "1\t2\t3\t3\t4\t5"
-print(tabbedString)
+tabbed_string = "1\t2\t3\t3\t4\t5"
+print(tabbed_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 # or
@@ -11,7 +11,7 @@ print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 print("""The pet shop owner said "No, no, \
 'e's uh,...he's resting". """)
 
-anotherSplitString = """This string has been \
+another_split_string = """This string has been \
 split over \
 several \
 lines"""
@@ -19,4 +19,4 @@ lines"""
 print("C:\\Users\\timbuchalka\\notes.txt")
 print(r"C:\Users\timbuchalka\notes.txt")
 
-print(anotherSplitString)
+print(another_split_string)
