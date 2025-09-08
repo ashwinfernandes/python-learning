6e3e8a8b77aadd04a15dc77f263a29bde25ea660
ashwinfernandes <ashtroid06@gmail.com>
2025-09-08 13:56:39 +0200

    coding exercise 13

---
+++ coding_exercise13.py
@@ -0,0 +1,35 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+# write your code here
+for x in data:
+    split_data = x.split(' - ')
+    if split_data[1].casefold() == "flower":
+        flowers.append(split_data[0])
+    else:
+        shrubs.append(split_data[0])
+
+print(flowers)
+print(shrubs)
