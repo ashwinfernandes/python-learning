9232b0a9f242d85a4ee6c52635f4ba6dc75729fd
ashwinfernandes <ashtroid06@gmail.com>
2025-11-13 16:58:22 +0100

    minor changes

--- summary_challenge1.py
+++ summary_challenge1.py
@@ -1,13 +1,13 @@
 # print a number of options and choose one of them, input 0 to quit
 options = ["Learn Python", "Learn Java", "Go swimming", "Have dinner", "Go to bed", "Drink coffee"]
 
-x = 10
+x = len(options) + 1
 while x != 0:
     if 0 < x <= len(options):
         print("Your choice is {}".format(x))
     else:
         print("Please choose an option from the list below:")
-        for i in range(0, len(options)):
+        for i in range(len(options)):
             print("{}.\t{}".format(i + 1,options[i]))
         else:
             print("0.\tExit")
