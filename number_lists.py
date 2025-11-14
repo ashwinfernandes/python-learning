1be4b9ed29b4e335762339ce230f0c61309aa5c3
ashwinfernandes <ashtroid06@gmail.com>
2025-11-14 11:27:30 +0100

    list - creating lists, concatenation, duplicating lists

--- number_lists.py
+++ number_lists.py
@@ -1,12 +1,36 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd) # extends even with elements of odd [2, 4, 6, 8, 1, 3, 5, 7, 9]
-print(even)
-another_even = even
-print(another_even)
+numbers = even + odd # concatenates 2 lists
+print(numbers)
 
-even.sort() # Sorts in ascending order. Mutates original list
-even.sort(reverse=True) # Sorts in descending order. Mutates original list
-print(even) # shows result of mutation
-print(another_even) # same as even
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
+number_string = "432985617"
+digits = sorted(number_string) # creates a list of characters
+print(digits)
+print(type(digits)) # will be a list
+
+digits = list(number_string) # using list initializer
+print(digits)
+
+# using list initializer to duplicate lists
+more_numbers = list(numbers)
+print(more_numbers)
+print(numbers is more_numbers) # False. Checks whether the lists are the same (reference the same object)
+print(numbers == more_numbers) # True. Checks whether the list items are the same.more_numbers = list(numbers)
+
+# using slice to duplicate lists
+more_numbers = numbers[:]
+print(more_numbers)
+print(numbers is more_numbers) # False. Checks whether the lists are the same (reference the same object)
+print(numbers == more_numbers) # True. Checks whether the list items are the same.more_numbers = numbers[:]
+
+# using copy method
+more_numbers = numbers.copy()
+print(more_numbers)
+print(numbers is more_numbers) # False. Checks whether the lists are the same (reference the same object)
+print(numbers == more_numbers) # True. Checks whether the list items are the same.
