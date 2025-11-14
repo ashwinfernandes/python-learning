f5a19ed85c2949b3f34053949fc727f8005fff4c
ashwinfernandes <ashtroid06@gmail.com>
2025-11-14 15:50:48 +0100

    change nospam approach 3, to use end=", " in the print function

--- nospam.py
+++ nospam.py
@@ -12,13 +12,13 @@ menu = [
 # Goal: print the meals with spam removed
 
 # Approach 1: Remove spam from the list and print it
-for meal in menu:
-    top_index = len(meal) - 1
-    for index, item in enumerate(reversed(meal)):
-        reversed_index = top_index - index
-        if item == "spam":
-            del meal[reversed_index]
-    print(meal)
+# for meal in menu:
+#     top_index = len(meal) - 1
+#     for index, item in enumerate(reversed(meal)):
+#         reversed_index = top_index - index
+#         if item == "spam":
+#             del meal[reversed_index]
+#     print(meal)
 
 # Approach 2: Create temporary local lists
 # for menu_item in menu:
@@ -29,15 +29,9 @@ for meal in menu:
 #         list_without_spam.append(item)
 #     print(list_without_spam)
 
-# Approach 3: Print the menu without spam without removing it from the list. Print simple formatted strings
-# for meal in menu:
-#     list_string = "["
-#     for index, item in enumerate(meal):
-#         if item == "spam":
-#             continue
-#         list_string+= item
-#         list_string+=','
-#     list_string+= "]"
-#     list_string = list_string.replace(",,",",")
-#     list_string = list_string.replace(",]","]")
-#     print(list_string)
+# Approach 3: Print the menu without spam, without removing it from the list. Print simple formatted strings
+for meal in menu:
+    for index, item in enumerate(meal):
+        if item != "spam":
+            print(item, end=", ")
+    print()
