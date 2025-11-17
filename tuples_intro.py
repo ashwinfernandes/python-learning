d20c11f2dac045360bf160ebda130b680c96e55e
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 12:35:08 +0100

    unpacking tuples continued

--- tuples_intro.py
+++ tuples_intro.py
@@ -4,13 +4,15 @@ budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the lightning", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
 
-metallica2 = list(metallica)
-print(metallica2)
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
 
-metallica2[0] = "Master of puppets"
-print(metallica2)
+# unpacking tuple into variables compared to indexing, makes it
+# easier to work with them and avoid mistakes due to wrong indexing
+name, length, width, height, price = table
+print(length * width)
