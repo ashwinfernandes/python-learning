096deb38b4ac61ad09b88d4fddb124a2e5d1f1a3
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 16:42:02 +0100

    function example - palindrome

--- functions.py
+++ functions.py
@@ -3,14 +3,14 @@ def multiply(x, y):
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
-
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+word = input("Please enter a word to check: ")
+if is_palindrome(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
