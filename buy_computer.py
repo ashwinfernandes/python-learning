c775bda7923b3f0c9246508cf1b09b5d675d2c8c
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 15:42:49 +0100

    Change buy_computer to use tuples that includes a price for the item. Print the total prices of the items bought in the end.

--- buy_computer.py
+++ buy_computer.py
@@ -1,9 +1,9 @@
-available_parts = ["computer",
-                   "monitor",
-                   "keyboard",
-                   "mouse",
-                   "hdmi cable",
-                   "dvd drive"
+available_parts = [("computer", 500),
+                   ("monitor", 300),
+                   ("keyboard", 50),
+                   ("mouse", 15),
+                   ("hdmi cable", 6.99),
+                   ("dvd drive", 23.99)
                    ]
 #valid_choices = [str(i) for i in range( 1, len(available_parts) + 1)]
 valid_choices = []
@@ -27,9 +27,10 @@ while current_choice != '0':
         print("Your list now contains: {}".format(computer_parts))
     else:
         print("Please add options from the list below:")
-        for number, part in enumerate(available_parts):
-            print("{0}: {1}".format(number + 1, part))
+        for number, (part, price) in enumerate(available_parts):
+            print("{0}: {1} {2}€".format(number + 1, part, price))
         print("0: to finish")
     current_choice = input()
 
 print(computer_parts)
+print("total amount: {0:5.2f}€".format(sum([price for (part, price) in computer_parts])))
