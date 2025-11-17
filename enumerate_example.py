11195076cb4a04936c04ade704364c0ac06d75a1
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 12:24:31 +0100

    tuple example - enumerate function (with comments added)

--- enumerate_example.py
+++ enumerate_example.py
@@ -1,8 +1,8 @@
 # for index, character in enumerate("abcdefgh"):
 #     print(index, character)
 
-for t in enumerate("abcdefgh"):
-    index, character = t
+for t in enumerate("abcdefgh"): # enumerating returns a tuple t
+    index, character = t # t is unpacked into individual variables index and character
     print(index, character)
 
 index, character = (0, 'a')
