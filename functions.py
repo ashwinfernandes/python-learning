32d2bd5db9521bf1555a2dcec0eb5ab46ef8a1dc
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 16:58:28 +0100

    function example - palindrome sentence

--- functions.py
+++ functions.py
@@ -9,8 +9,19 @@ def is_palindrome(string):
     return string[::-1].casefold() == string.casefold()
 
 
-word = input("Please enter a word to check: ")
-if is_palindrome(word):
-    print("'{}' is a palindrome".format(word))
+def is_palindrome_sentence(string):
+    palindrome_sentence = "".join([char if char.isalnum() else '' for char in string.casefold()])
+    return palindrome_sentence[::-1] == palindrome_sentence
+
+
+# word = input("Please enter a word to check: ")
+# if is_palindrome(word):
+#     print("'{}' is a palindrome".format(word))
+# else:
+#     print("'{}' is not a palindrome".format(word))
+
+sentence = input("Please enter a sentence to check: ")
+if is_palindrome_sentence(sentence):
+    print("'{}' is a palindrome".format(sentence))
 else:
-    print("'{}' is not a palindrome".format(word))
+    print("'{}' is not a palindrome".format(sentence))
