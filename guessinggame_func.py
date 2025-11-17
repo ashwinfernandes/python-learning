386e066f5a55e69d976a02f5f9c5bd68e802e92a
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 17:19:15 +0100

    guessing game, input validation using functions

---
+++ guessinggame_func.py
@@ -0,0 +1,38 @@
+import random
+
+
+def get_integer(prompt):
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+        print("{0} is not a valid number.".format(temp))
+
+
+highest = 1000
+answer = random.randint(1, highest)
+print(answer) # TODO: Remove after testing
+
+print("Please guess number between 1 and {}: ".format(highest))
+
+attempts = 3
+guess = None
+while guess != answer:
+    guess = get_integer(": ")
+
+    if guess == 0:
+        print("Game over!")
+        break
+
+    attempts -= 1
+    if guess == answer:
+        print("Well done, you guessed it!")
+        break
+
+    if attempts == 0:
+        print("Sorry, you have run out of guesses!")
+        break
+    elif guess < answer:
+        print("Please guess higher")
+    else:   # guess must be greater than answer
+        print("Please guess lower")
