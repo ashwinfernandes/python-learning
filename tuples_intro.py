6cc7a2d92c41b6af88a6352a61806df844b2fb85
ashwinfernandes <ashtroid06@gmail.com>
2025-11-17 13:13:18 +0100

    unpacking tuples continued

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,18 +1,19 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the lightning", "Metallica", 1984
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the lightning", "Metallica", 1984),
+          ]
 
-title, artist, year = metallica
-print(title)
-print(artist)
-print(year)
+print(len(albums))
 
-table = ("Coffee table", 200, 100, 75, 34.50)
-print(table[1] * table[2])
+# unpacking tuples directly
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
 
-# unpacking tuple into variables compared to indexing, makes it
-# easier to work with them and avoid mistakes due to wrong indexing
-name, length, width, height, price = table
-print(length * width)
+# unpacking tuples within the for
+for album in albums:
+    name, artist, year = album
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
