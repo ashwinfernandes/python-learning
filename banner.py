1f428e51faee6ecb77754ff396759fa43f2e1f92
ashwinfernandes <ashtroid06@gmail.com>
2025-11-18 10:02:35 +0100

    functions - key word arguments

--- banner.py
+++ banner.py
@@ -1,4 +1,4 @@
-def banner_text(text, screen_width = 80):
+def banner_text(text=" ", screen_width = 80):
     if not screen_width.is_integer():
         raise TypeError("The text width is not of type int. Expected integer. ")
 
@@ -12,14 +12,15 @@ def banner_text(text, screen_width = 80):
         output_string = "**{0}**".format(text.center(screen_width - 4))
         print(output_string)
 
-screen_width = 80
-banner_text("*", screen_width)
-banner_text("Always look on the light side of life", screen_width)
-banner_text("If life seems jolly rotten", screen_width)
-banner_text("There's something you've forgotten", screen_width)
-banner_text("And that's to laugh and smile and dance and sing", screen_width)
-banner_text("When you're feeling in the dumps", screen_width)
-banner_text("Don't be silly chumps", screen_width)
-banner_text("Just purse your lips and whistle, that's the thing", screen_width)
-banner_text("And...always look on the light side of life...", screen_width)
-banner_text("*", screen_width)
+
+banner_text("*", 60)
+banner_text("Always look on the light side of life", 60)
+banner_text("If life seems jolly rotten", 60)
+banner_text("There's something you've forgotten", 60)
+banner_text("And that's to laugh and smile and dance and sing,", 60)
+banner_text(screen_width=60)
+banner_text("When you're feeling in the dumps", 60)
+banner_text("Don't be silly chumps", 60)
+banner_text("Just purse your lips and whistle, that's the thing", 60)
+banner_text("And...always look on the light side of life...", 60)
+banner_text("*", 60)
