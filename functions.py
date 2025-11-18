f6260910206800e21550fcf2f3984a9438396f9a
ashwinfernandes <ashtroid06@gmail.com>
2025-11-18 11:50:29 +0100

    documenting functions using Docstrings

--- functions.py
+++ functions.py
@@ -1,24 +1,42 @@
 def multiply(x, y):
+    """
+    Multiplies two items and returns the result.
+    :param x: The multiplicand.
+    :param y: The multiplier.
+    :return: The product after multiplying the two items.
+    """
     result = x * y
     return result
 
 
 def is_palindrome(string):
-    # backwards = string[::-1]
-    # return backwards == string
+    """
+    Verifies whether the provided string is a palindrome or not.
+
+    A palindrome is a string that reads the same forwards as backwards.
+
+    The function is case-insensitive.
+
+    :param string: The string to be verified.
+    :return: True if `string` is a palindrome, False otherwise.
+    """
     return string[::-1].casefold() == string.casefold()
 
 
-def is_palindrome_sentence(string):
-    palindrome_sentence = "".join([char if char.isalnum() else '' for char in string.casefold()])
+def is_palindrome_sentence(sentence):
+    """
+    Verifies whether the provided sentence is a palindrome or not.
+
+    The sentence can be combination of alphanumeric characters.
+
+    The function ignores whitespace, capitalization and
+    punctuation in the sentence.
+
+    :param sentence: The sentence to be verified.
+    :return: True if `sentence` is a palindrome, False otherwise.
+    """
+    palindrome_sentence = "".join([char if char.isalnum() else '' for char in sentence.casefold()])
     return is_palindrome(palindrome_sentence)
-
-
-# word = input("Please enter a word to check: ")
-# if is_palindrome(word):
-#     print("'{}' is a palindrome".format(word))
-# else:
-#     print("'{}' is not a palindrome".format(word))
 
 sentence = input("Please enter a sentence to check: ")
 if is_palindrome_sentence(sentence):
