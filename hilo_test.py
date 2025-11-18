1945314a5cae0543389ec9e804645b0f2ccbfdd3
ashwinfernandes <ashtroid06@gmail.com>
2025-11-18 17:01:24 +0100

    Test binary search. Find max numbers of guesses based on high value

--- hilo_test.py
+++ hilo_test.py
@@ -1,5 +1,9 @@
 LOW = 1
-HIGH = 1000
+HIGH = 1000 # needs at most x guesses to guess the right value,
+# where x is the binary exponent that results in the exact high value
+# or is the next higher exponent
+# e.g. high value of 512 would require at most 2^9 guesses
+# high value of 513 would require at most 2^10 guesses
 
 # print("Please think of a number between 1 and 1000")
 # input("Press ENTER to start")
@@ -8,13 +12,11 @@ HIGH = 1000
 def guess_binary(answer, low, high):
     guesses = 1
     while True:
-        # print("\tGuessing in the range {} to {}".format(low, high))
         guess = low + (high - low) // 2
 
         if guess < answer:
             # I have to guess higher.  The low end of the range becomes 1 greater than the guess.
             low = guess + 1
-        # elif high_low == "l":
         elif guess > answer:
             # I have to guess lower.  The high end of the range becomes 1 less than the guess.
             high = guess - 1
@@ -24,6 +26,16 @@ def guess_binary(answer, low, high):
         guesses += 1
 
 
+correct_count = 0
+max_guesses = 0
 for number in range(LOW, HIGH + 1):
     number_of_guesses = guess_binary(number, LOW, HIGH)
     print("{} guessed in {}".format(number, number_of_guesses))
+
+    if number_of_guesses > max_guesses:
+        max_guesses, correct_count = number_of_guesses, 1
+    elif number_of_guesses == max_guesses:
+        correct_count += 1
+
+print("I guessed without being told {} times. Max {} guesses."
+      .format(correct_count, max_guesses))
