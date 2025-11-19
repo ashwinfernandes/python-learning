97fab1b4164897a5d7cf52c58caabc0b050d05d3
ashwinfernandes <ashtroid06@gmail.com>
2025-11-19 16:38:01 +0100

    Counting characters in text - coding exercise 20

---
+++ counting_character.py
@@ -0,0 +1,16 @@
+# We need an empty dictionary, to store and display the letter frequencies.
+word_count = {}
+
+# Text string
+text = "Later in the course, you'll see how to use the collections Counter class."
+
+# Your code goes here ...
+for c in text.casefold():
+    if not c.isalnum():
+        continue
+
+    word_count[c] = word_count.setdefault(c, 0) + 1
+
+# Printing the dictionary
+for letter, count in sorted(word_count.items()):
+    print(letter, count)
