8cc8f0446086d9be8d57245266d251657ce21637
ashwinfernandes <ashtroid06@gmail.com>
2025-11-19 16:16:21 +0100

    Add missing ingredients with their quantities to a shopping list - challenge

--- meal_planner.py
+++ meal_planner.py
@@ -1,9 +1,22 @@
 from contents import pantry, recipes
 
+
+def add_ingredients_to_shopping_cart(shopping_cart: dict, item: (str, int)):
+    """
+    Add a tuple `item` containing the ingredient and quantity to a
+    `shopping_cart` dictionary. The quantity gets added to the existing ingredient
+    if it's already in `the shopping cart` or a new entry is created.
+    """
+    food_item, quantity = item
+    new_quantity = shopping_cart.get(food_item, 0) + quantity
+    shopping_cart[food_item] = new_quantity
+
+
 print("")
 # display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
 # - using dictionary comprehension
 display_dict = {}
+shopping_list = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
 
@@ -23,8 +36,16 @@ while True:
         print(f"You have selected {selected_item}")
         print("checking ingredients...")
         ingredients = recipes[selected_item]
-        for ingredient in ingredients:
-            if ingredient in pantry:
+
+        for ingredient, require_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(ingredient, 0)
+            if require_quantity <= quantity_in_pantry:
                 print(f"\t{ingredient} OK.")
             else:
-                print(f"\tYou don't have a necessary ingredient: {ingredient}")
+                quantity_to_buy = require_quantity - quantity_in_pantry
+                print(f"\tYou need to buy {quantity_to_buy} of {ingredient}")
+                print(f"Adding {quantity_to_buy} of {ingredient} to shopping cart...")
+                add_ingredients_to_shopping_cart(shopping_list, (ingredient, quantity_to_buy))
+
+for things in shopping_list.items():
+    print(things)
