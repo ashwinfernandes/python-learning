145e465952fecc97b454cbdab8bb0e9b50ba93bc
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 16:07:50 +0100

    Code that demonstrates how strip() works

--- text-files/stripping.py
+++ text-files/stripping.py
@@ -5,5 +5,21 @@ with open(filename) as poem:
 print(first)
 
 chars = "' Twasebv"
-no_apostrophe = first.strip(chars)
-print(no_apostrophe)
+# no_apostrophe = first.strip(chars)
+# print(no_apostrophe)
+
+# breaks after finding a non-matching character in chars
+for character in first:
+    if character in chars:
+        print(f'removing "{character}"')
+    else:
+        break
+
+print('*' * 80)
+
+# breaks after finding (in reverse) a non-matching characters in chars
+for character in first[::-1]: # process backwards
+    if character in chars:
+        print(f'removing "{character}"')
+    else:
+        break
