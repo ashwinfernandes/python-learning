986f532b868b696c7dbc226bd54a4ba089914a48
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 11:06:59 +0100

    Use difference_update instead of discard to remove restricted items from packing list in a single step

--- packing_list.py
+++ packing_list.py
@@ -48,10 +48,10 @@ while mode not in travel_mode:
 
 if mode == "2":
     # travelling by plane, remove restricted items
-    for restricted_item in restricted_items:
-        items.discard(restricted_item)
-        # remove will crash the program
-        # items.remove(restricted_item)
+    # for restricted_item in restricted_items:
+    #     items.discard(restricted_item)
+    # items -= restricted_items
+    items.difference_update(restricted_items)
 
 # print the packing list
 print("You need to pack:")
