360eed56a9f0130985fc2517c2874361f3d501e3
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 15:50:51 +0100

    using read() file contents into a single string

--- text-files/read_poem.py
+++ text-files/read_poem.py
@@ -6,13 +6,17 @@
 #
 # jabber.close()
 
-with open('Jabberwocky.txt', 'r') as jabber:
-    # for line in jabber:
-    #     print(line.rstrip())
-    lines = jabber.readlines()
+# with open('Jabberwocky.txt', 'r') as jabber:
+#     # for line in jabber:
+#     #     print(line.rstrip())
+#     lines = jabber.readlines()
+#
+# print(lines)
+# print(lines[-1:])
 
-print(lines)
-print(lines[-1:])
-
-for line in reversed(lines):
-    print(line, end='') # do some processing in reverse order
+# with open('Jabberwocky.txt') as jabber:
+#     text = jabber.read()
+#
+# # print(text)
+# for character in reversed(text):
+#     print(character, end='')
