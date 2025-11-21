93a7a9aef277ca4e0ed6ad02cbadbb86aa6da933
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 09:41:12 +0100

    Set union contd.

--- set_union.py
+++ set_union.py
@@ -15,10 +15,14 @@ from prescription_data import adverse_interactions
 
 meds_to_watch = set()
 
-for interaction in adverse_interactions:
+# for interaction in adverse_interactions:
     # meds_to_watch = meds_to_watch.union(interaction)
     # meds_to_watch = meds_to_watch | interaction
     # meds_to_watch.update(interaction)
-    meds_to_watch |= interaction
+    # meds_to_watch |= interaction
+
+meds_to_watch.update(*adverse_interactions)
 
 print(sorted(meds_to_watch))
+print(*sorted(meds_to_watch), sep="\n") # unpacks the set items and
+# prints each of them on the console output with `\n`as the separator
