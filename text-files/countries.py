e065455cf3b77d010ebc7e929bfed7f70ac4676b
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 16:55:17 +0100

    cache contents from file in dictionary

--- text-files/countries.py
+++ text-files/countries.py
@@ -1,6 +1,22 @@
 input_filename = 'country_info.txt'
 
+countries = {}
 with open(input_filename) as country_file:
+    country_file.readline() # skips the headings
     for row in country_file:
         data = row.strip('\n').split('|')
-        print(data)
+        country, capital, code, code3, dialing, timezone, currency = data
+        # print(country, capital, code, code3, dialing, timezone, currency, sep='\n\t')
+        country_dict = {
+            'name': country,
+            'capital': capital,
+            'country_code': code,
+            'cc3': code3,
+            'dialing_code': dialing,
+            'timezone': timezone,
+            'currency': currency
+        }
+        print(country_dict)
+        countries[country.casefold()] = country_dict
+
+print(countries)
