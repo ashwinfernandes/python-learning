d3de785255b6716ac9d9b64613ef7bcf28d79019
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 15:37:53 +0100

    using with expression to read a file

--- text-files/read_poem.py
+++ text-files/read_poem.py
@@ -1,7 +1,11 @@
-jabber = open("Jabberwocky.txt", 'r')
+# jabber = open("Jabberwocky.txt", 'r')
+#
+# for line in jabber:
+#     print(line.rstrip())
+#     # print(len(line))
+#
+# jabber.close()
 
-for line in jabber:
-    print(line.rstrip())
-    # print(len(line))
-
-jabber.close()
+with open('Jabberwocky.txt', 'r') as jabber:
+    for line in jabber:
+        print(line.rstrip())
