10bf4f358790d3e85827a108830d1b923f93800d
ashwinfernandes <ashtroid06@gmail.com>
2025-11-21 16:16:29 +0100

    Code that demonstrates how strip() works, write own removeprefix() and removesuffix() functions

--- text-files/stripping.py
+++ text-files/stripping.py
@@ -1,3 +1,16 @@
+def removeprefix(string:str, prefix:str) -> str:
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    else:
+        return string[:] # returns a copy of `string`
+
+def removesuffix(string:str, suffix: str) -> str:
+    if suffix and string.endswith(suffix): # suffix='' should not call string[:-0]
+        return string[:-len(suffix)]
+    else:
+        return string[:]
+
+
 filename = 'Jabberwocky.txt'
 with open(filename) as poem:
     first = poem.readline().rstrip()
@@ -23,3 +36,12 @@ for character in first[::-1]: # process backwards
         print(f'removing "{character}"')
     else:
         break
+
+print('*' * 80)
+
+# twas_removed = first.removeprefix("'Twas")
+twas_removed = removeprefix(first, "'Twas")
+print(twas_removed)
+# toves_removed = first.removesuffix('toves')
+toves_removed = removesuffix(first, 'toves')
+print(toves_removed)
