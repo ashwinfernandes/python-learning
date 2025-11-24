f0eb44bd14220070aa3d259c0e26e4c0c71a27a0
ashwinfernandes <ashtroid06@gmail.com>
2025-11-24 15:20:19 +0100

    modify the code to manually change the case of the headings and provide it to the DictReader to use as keys

--- text-files/dict_reader_challenge.py
+++ text-files/dict_reader_challenge.py
@@ -2,13 +2,21 @@ import csv
 
 input_filename = 'country_info.txt'
 
+dialect = csv.excel
+dialect.delimiter = '|'
+
 countries = {}
 with open(input_filename, encoding='utf-8', newline='') as country_file:
-    reader = csv.DictReader(country_file, delimiter="|")
+    # Get the column headings from the first line of the file
+    headings = country_file.readline().strip('\n').split(dialect.delimiter)
+    for index, heading in enumerate(headings):
+        headings[index] = heading.casefold()
+
+    reader = csv.DictReader(country_file, dialect=dialect, fieldnames=headings)
     for row in reader:
-        country = row['Country']
+        country = row['country']
         countries[country.casefold()] = row
-        countries[row['CC'].casefold()] = row
+        countries[row['cc'].casefold()] = row
 
 print(countries)
 
@@ -16,8 +24,8 @@ while True:
     country = input("Please enter the name for a country (type quit to exit): ")
     country_key = country.casefold()
     if country_key in countries:
-        print(f"The capital of the {country} is {countries[country_key]['Capital']}")
-    elif country_key == 'quit':
+        print(f"The capital of the {country} is {countries[country_key]['capital']}")
+    elif country_key.casefold() == 'quit':
         break
     else:
         print(f"Could not find the capital of {country}")
