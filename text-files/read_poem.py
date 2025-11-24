48cdf1594ef5f7cddef7bbdaf4775179e69ce115
ashwinfernandes <ashtroid06@gmail.com>
2025-11-24 11:44:41 +0100

    read file by providing the encoding

--- text-files/read_poem.py
+++ text-files/read_poem.py
@@ -1,37 +1,3 @@
-# jabber = open("Jabberwocky.txt", 'r')
-#
-# for line in jabber:
-#     print(line.rstrip())
-#     # print(len(line))
-#
-# jabber.close()
-
-# with open('Jabberwocky.txt', 'r') as jabber:
-#     # for line in jabber:
-#     #     print(line.rstrip())
-#     lines = jabber.readlines()
-#
-# print(lines)
-# print(lines[-1:])
-
-# with open('Jabberwocky.txt') as jabber:
-#     text = jabber.read()
-#
-# # print(text)
-# for character in reversed(text):
-#     print(character, end='')
-
-with  open('Jabberwocky.txt') as jabber:
-    while True:
-        line = jabber.readline().rstrip()
-        print(line)
-        if 'jubjub' in line.casefold():
-            break
-
-print('*' * 80)
-
-with open('Jabberwocky.txt') as jabber:
+with open('Jabberwocky.txt', encoding='UTF-8') as jabber:
     for line in jabber:
         print(line.rstrip())
-        if 'jubjub' in line.casefold():
-            break
