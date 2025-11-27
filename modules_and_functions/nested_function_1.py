e066a13808e77bbb5196a432032196c0524d5deb
ashwinfernandes <ashtroid06@gmail.com>
2025-11-27 14:08:42 +0100

    how to use nonlocal to update variables from the enclosing outer scope

--- modules_and_functions/nested_function_1.py
+++ modules_and_functions/nested_function_1.py
@@ -4,9 +4,12 @@ def greet_pythons(items: list) -> None:
     print(f'local namespace in `greet_pythons`(1): {locals()}')
 
     def make_greeting(item: str) -> str:
+        nonlocal greeting
         print(f'local namespace in `make_greeting`(1): {locals()}')
-        greeting = 'Hola' # no longer a free variable. A new variable # is created in the local scope
-        print(f'The ID of `greeting`in `make_greeting` is {id(greeting)}')
+        greeting = 'Hola'  # by declaring as nonlocal, the variable now refers to the variable from the outer scope
+        print(
+            f'The ID of `greeting`in `make_greeting` is {id(greeting)}')  # id still changes because strings are immutable,
+        # rebinding it to a new value, changes the id
         print(f'local namespace in `make_greeting`(2): {locals()}')
         return f'{greeting} {item}'
 
@@ -17,6 +20,6 @@ def greet_pythons(items: list) -> None:
     print(f'local namespace in `greet_pythons`(2): {locals()}')
 
 
-names = ['John']#, 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
+names = ['John']  # , 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
 
 greet_pythons(names)
