30af34d706a7409a04983ac2d82788eee7bc13c0
ashwinfernandes <ashtroid06@gmail.com>
2025-11-28 11:12:02 +0100

    time object demo

---
+++ modules_and_functions/time_example.py
@@ -0,0 +1,17 @@
+from datetime import time, date
+
+meeting = time(hour=11, minute=15, second=0)
+print(meeting)
+
+end_time = time(hour=12, minute=30)
+print(end_time)
+
+# print(end_time - meeting) # is not allowed
+# iso_time = 'T11:15:00'
+iso_time = '11:15:00'
+_time = time.fromisoformat(iso_time)
+print(_time)
+
+iso_date = '2022-05-10'
+_date = date.fromisoformat(iso_date)
+print(_date)
