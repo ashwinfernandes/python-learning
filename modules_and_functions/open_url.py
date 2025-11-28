437d7e31cee43429d76fe81c6770118aecb02559
ashwinfernandes <ashtroid06@gmail.com>
2025-11-28 10:37:36 +0100

    module that opens a web url using the webbrowser module

---
+++ modules_and_functions/open_url.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+# webbrowser.open('https://docs.python.org/3.13/library/webbrowser')
+
+chrome = webbrowser.get(using='chrome')
+chrome.open('https://learnprogramming.academy/courses')
