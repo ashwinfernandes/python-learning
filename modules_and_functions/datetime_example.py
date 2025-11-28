380f00978bbea4477166e4433eaab0b4cc4144a9
ashwinfernandes <ashtroid06@gmail.com>
2025-11-28 15:20:06 +0100

    datetime contd. today vs now vs utcnow

--- modules_and_functions/datetime_example.py
+++ modules_and_functions/datetime_example.py
@@ -1,10 +1,8 @@
 import datetime as dt
 from datetime import datetime
 
-today = dt.datetime.today()
 now = datetime.now()
 utc_now = datetime.utcnow() # utcnow is deprecated
 
-print(today)
 print(now)
 print(utc_now)
