ce47aedcc6188b12080e9fbf5f8a1fbca4878879
ashwinfernandes <ashtroid06@gmail.com>
2025-12-03 16:49:24 +0100

    blackjack program - some fixes, using global keyword to modify global variables in functions

--- learn_tkinter/blackjack.py
+++ learn_tkinter/blackjack.py
@@ -5,7 +5,6 @@ try:
 except ImportError: # python 2
     import Tkinter as tkinter
 
-main_window = tkinter.Tk()
 
 
 def load_images(card_images):
@@ -34,7 +33,7 @@ def load_images(card_images):
 
 def deal_card(frame):
     # Pop the next card off the top of the deck
-    next_card = deck.pop()
+    next_card = deck.pop(0)
     # add the image tp a Label and display the label
     tkinter.Label(frame, image=next_card[1], relief="raised").pack(side="left")
     # now return the card's face value
@@ -42,15 +41,33 @@ def deal_card(frame):
 
 
 def deal_dealer():
-    return deal_card(dealer_card_frame)
+    deal_card(dealer_card_frame)
+
 
 def deal_player():
-    return deal_card(player_card_frame)
+    global player_score
+    global player_ace
+    card_value = deal_card(player_card_frame)[0]
+    if card_value == 1 and not player_ace:
+        player_ace = True
+        card_value = 11
+    player_score += card_value
+    # if we would bust, check if there is an ace and subtract
+    if player_score > 21 and player_ace:
+        player_score -= 10
+        player_ace = False
+    player_score_label.set(player_score)
+    if player_score > 21:
+        result_text.set("Dealer wins!")
+    print(locals())
 
+
+main_window = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
 main_window.title("Black Jack")
 main_window.geometry("640x480")
+main_window.configure(background="green")
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(main_window, textvariable=result_text)
@@ -67,6 +84,9 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
+player_score = 0
+player_ace = False
+
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 #embedded frame to hold the card images
