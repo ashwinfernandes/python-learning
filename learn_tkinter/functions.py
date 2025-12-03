08d33623719ac010996d3ce13ca66e08c546197b
ashwinfernandes <ashtroid06@gmail.com>
2025-12-03 11:45:27 +0100

    functions contd.

--- learn_tkinter/functions.py
+++ learn_tkinter/functions.py
@@ -5,21 +5,36 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+def center_text(*args, sep=' '):
     text = ""
     for arg in args:
         text += str(arg) + sep
     text = text.removesuffix(sep)
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
 
-with open("centered", mode='w') as centered_file:
-    # call the function
-    center_text("Spam and eggs", file=centered_file)
-    center_text("Spam, spam and eggs", file=centered_file)
-    center_text(12, file=centered_file)
-    center_text("Spam, spam, spam and spam", file=centered_file)
+# call the function
+# s1 = center_text("Spam and eggs")
+# print(s1)
+# s2 = center_text("Spam, spam and eggs")
+# print(s2)
+# s3 = center_text(12)
+# print(s3)
+# s4 = center_text("Spam, spam, spam and spam")
+# print(s4)
+# s5 = center_text("first", "second", 3, 4, "spam", sep=':')
+# print(s5)
+#
+# print("=" + str(12 * 3))
+# print(sorted(['b', 'd', 'c', 'a']))
 
-    # print("first", "second", 3, 4, "spam")
-    center_text("first", "second", 3, 4, "spam", sep=':', file=centered_file)
+with open("menu", 'w') as menu:
+    s1 = center_text("Spam and eggs")
+    print(s1, file=menu)
+    s2 = center_text("Spam, spam and eggs")
+    print(s2, file=menu)
+    print(center_text(12), file=menu)
+    print(center_text("Spam, spam, spam and spam"), file=menu)
+    s5 = center_text("first", "second", 3, 4, "spam", sep=':')
+    print(s5, file=menu)
