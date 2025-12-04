39e11b4c92f5ad51e7513b2f7ed57fea9d6cb6b0
ashwinfernandes <ashtroid06@gmail.com>
2025-12-04 12:57:24 +0100

    modules import and purpose of using "_" while naming functions or variables

--- learn_tkinter/import_test.py
+++ learn_tkinter/import_test.py
@@ -1,5 +1,15 @@
 import blackjack
 
-print(__name__)
-blackjack.play()
-print(blackjack.cards)
+# from blackjack import *  # imports everything from the module blackjack except
+# everything that begins with an '_' i.e. _deal_card() will not be imported and
+# therefore cannot be used here
+
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# blackjack.play()
+
+personal_details = ("Tim", 24, "Australia")
+
+# using _ as a variable name when the value is not interesting and can be ignored
+name, _, country = personal_details
+print(name, country)
+print(_)
