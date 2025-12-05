31e0b21425c629ff60e4527888c3ad6d16e41bf9
ashwinfernandes <ashtroid06@gmail.com>
2025-12-05 17:09:30 +0100

    oop RPG demo - add behavior to sub class

--- oop_game/main.py
+++ oop_game/main.py
@@ -1,10 +1,14 @@
 from enemy import Enemy, Troll
 
-ugly_troll = Troll()
+ugly_troll = Troll("Pug")
 print(f"Ugly troll - {ugly_troll}")
 
-another_troll = Troll("Ug", 18, 1)
+another_troll = Troll("Ug")
 print(f"Another troll - {another_troll}")
 
-brother = Enemy("Urg", 23)
+brother = Troll("Urg")
 print(brother)
+
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
