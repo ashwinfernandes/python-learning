f0a2538b3398b6924ee379d55a6ff8c428f0ad68
ashwinfernandes <ashtroid06@gmail.com>
2025-12-05 17:32:49 +0100

    oop RPG demo - create a second sub-class and add extra functionality

--- oop_game/enemy.py
+++ oop_game/enemy.py
@@ -3,7 +3,9 @@ class Enemy:
     def __init__(self, name="Enemy", hit_points=0, lives=1):
         self.name = name
         self.hit_points = hit_points
+        self.original_hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,6 +14,12 @@ class Enemy:
             print(f"I took {damage} point damage and have {self.hit_points} left")
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print(f"{self.name} lost a life")
+                self.hit_points = self.original_hit_points
+            else:
+                print(f"{self.name} is dead")
+                self.alive = False
 
     def __str__(self):
         return "Name : {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
@@ -25,3 +33,9 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}. {0.name} stomp you.".format(self))
+
+
+class Vampyre(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
--- oop_game/main.py
+++ oop_game/main.py
@@ -1,10 +1,12 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampyre
 
 ugly_troll = Troll("Pug")
 print(f"Ugly troll - {ugly_troll}")
 
 another_troll = Troll("Ug")
 print(f"Another troll - {another_troll}")
+another_troll.take_damage(18)
+print(another_troll)
 
 brother = Troll("Urg")
 print(brother)
@@ -12,3 +14,16 @@ print(brother)
 ugly_troll.grunt()
 another_troll.grunt()
 brother.grunt()
+
+vlad = Vampyre("Vlad")
+print(vlad)
+vlad.take_damage(5)
+print(vlad)
+
+print("-" * 40)
+another_troll.take_damage(30)
+print(another_troll)
+
+while vlad.alive:
+    vlad.take_damage(1)
+    print(vlad)
