fbd8d1f46d123b506fe94b98258adfc391fc5a58
ashwinfernandes <ashtroid06@gmail.com>
2026-02-13 14:43:16 +0100

    polymorphism - calling print method with different types of parameters.

--- oop_game/main.py
+++ oop_game/main.py
@@ -1,39 +1,7 @@
-from enemy import Enemy, Troll, Vampyre, VampyreKing
+a = 3
+b = "Tim"
+c = 1, 2, 3
 
-# ugly_troll = Troll("Pug")
-# print(f"Ugly troll - {ugly_troll}")
-#
-# another_troll = Troll("Ug")
-# print(f"Another troll - {another_troll}")
-# another_troll.take_damage(18)
-# print(another_troll)
-#
-# brother = Troll("Urg")
-# print(brother)
-#
-# ugly_troll.grunt()
-# another_troll.grunt()
-# brother.grunt()
-#
-# vlad = Vampyre("Vlad")
-# print(vlad)
-# vlad.take_damage(5)
-# print(vlad)
-#
-# print("-" * 40)
-# another_troll.take_damage(30)
-# print(another_troll)
-#
-# # while vlad.alive:
-# #     vlad.take_damage(1)
-#
-# vlad._lives = 0
-# vlad._hit_points = 1
-# print(vlad)
-
-print("-" * 40)
-dracula = VampyreKing("Dracula")
-print(dracula)
-
-while dracula._alive:
-    dracula.take_damage(4)
+print(a)
+print(b)
+print(c)
