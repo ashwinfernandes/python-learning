c4e84a26df994147731315c26c3ee7fbdcf519e0
ashwinfernandes <ashtroid06@gmail.com>
2026-02-13 16:23:57 +0100

    polymorphism + composition - html example; write to file; challenge - add title to head

--- oop_game/html_doc.py
+++ oop_game/html_doc.py
@@ -10,43 +10,54 @@ class Tag(object):
 
     def display(self, file=None):
         print(self, file=file)
-        
-        
+
+
 class DocType(Tag):
-    
+
     def __init__(self):
-        super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd','')
-        self.end_tag = ''   # DOCTYPE doesn't have end tag
+        super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
+        self.end_tag = ''  # DOCTYPE doesn't have end tag
 
 
-class Head(Tag):
-    
-    def __init__(self):
-        super().__init__('head', '')
-        
-        
-class Body(Tag):
-    
-    def __init__(self):
-        super().__init__('body', '')    # body contents will be built up separately
-        self._body_contents = []
-        
-    def add_tag(self, name, contents):
+class ContentsTag(Tag):
+
+    def __init__(self, name, contents):
+        super().__init__(name, contents)
+        self._contents = []
+
+    def _add_tag(self, name, contents):
         new_tag = Tag(name, contents)
-        self._body_contents.append(new_tag)
-        
+        self._contents.append(new_tag)
+
     def display(self, file=None):
-        for tag in self._body_contents:
+        for tag in self._contents:
             self.contents += str(tag)
-            
+
         super().display(file=file)
+
+
+class Head(ContentsTag):
+
+    def __init__(self, title=None):
+        super().__init__('head', '')
+        if title:
+            super()._add_tag("title", title)
+
+
+class Body(ContentsTag):
+
+    def __init__(self):
+        super().__init__('body', '')  # body contents will be built up separately
+
+    def add_tag(self, name, contents):
+        super()._add_tag(name, contents)
 
 
 class HtmlDoc(object):
 
-    def __init__(self):
+    def __init__(self, title=None):
         self._doc_type = DocType()
-        self._head = Head()
+        self._head = Head(title=title)
         self._body = Body()
 
     def add_tag(self, name, contents):
@@ -61,8 +72,10 @@ class HtmlDoc(object):
 
 
 if __name__ == "__main__":
-    my_page = HtmlDoc()#
+    my_page = HtmlDoc(title='Document title')  #
     my_page.add_tag('h1', 'Main heading')
     my_page.add_tag('h2', 'sub-heading')
     my_page.add_tag('p', 'This is a paragraph that will appear on the page')
-    my_page.display()
+    # my_page.display()
+    with open('test.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
